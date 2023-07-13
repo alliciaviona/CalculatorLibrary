04fa5b41a85e7123578560a383bfbfc50a553f9d
Allicia Viona <alliciawn@gmail.com>
2023-07-13 15:42:59 +0700

    - Add public init to calculator class - Change methods inside calculator class protection level to public

--- Sources/Features/Calculator.swift
+++ Sources/Features/Calculator.swift
@@ -7,21 +7,23 @@
 
 import Foundation
 
-class Calculator {
+public class Calculator {
     
-    func add(valueA: Int, valueB: Int) -> Int {
+    public init() {}
+    
+    public func add(valueA: Int, valueB: Int) -> Int {
         return valueA + valueB
     }
     
-    func subtract(valueA: Int, valueB: Int) -> Int {
+    public func subtract(valueA: Int, valueB: Int) -> Int {
         return valueA - valueB
     }
     
-    func multiply(valueA: Int, valueB: Int) -> Int {
+    public func multiply(valueA: Int, valueB: Int) -> Int {
         return valueA * valueB
     }
     
-    func divide(valueToDivide: Int, divisor: Int) -> Int {
+    public func divide(valueToDivide: Int, divisor: Int) -> Int {
         return valueToDivide / divisor
     }
     
